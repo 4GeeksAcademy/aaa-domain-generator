0602ff9cbd6d2340851dc5eb0cac80c030da1ca9
Armando7101 <164431619+Armando7101@users.noreply.github.com>
2025-01-27 20:58:42 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pro = ["el", "la", "nuestro"];
+let adj = ["gran", "excelente", "espectacular"];
+let sus = ["atleta", "programador", "chef", "astronauta"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let dominios = [];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let p of pro) {
+  for (let a of adj) {
+    for (let n of sus) {
+      let dominio = `${p}${a}${n}.com`;
+      dominios.push(dominio);
+    }
+  }
+}
+
+const listaDeDominios = document.getElementById("lista-dominios");
+
+for (let dominio of dominios) {
+  let listItem = document.createElement("li");
+  listItem.textContent = dominio;
+  listaDeDominios.appendChild(listItem);
+}
